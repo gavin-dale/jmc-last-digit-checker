dde5e767e3f278c6dcaa69a19afbb4dc1320d5f5
gavin-dale <63865924+gavin-dale@users.noreply.github.com>
2021-02-19 11:53:20 -0700

    Add files via upload

---
+++ LastDigitChecker/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+
+        if(num1 < 10 || num1 > 1000 || num2 < 10 || num2 > 1000 || num3 < 10 || num3 > 1000){
+            return false;
+        }
+
+        int num1Last = num1%10;
+        int num2Last = num2%10;
+        int num3Last = num3%10;
+
+        if(num1Last == num2Last || num1Last == num3Last || num2Last == num3Last){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isValid(int num){
+       if(num > 10 && num < 1000){
+           return true;
+       } else {
+           return false;
+       }
+    }
+}
